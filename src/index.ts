c1516a2e0afd25e6005a901ecf99c14423b60473
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2023-11-29 18:24:10 +0530

    more injections, type safety

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import injectGlobals from './inject-globals'
 
-injectGlobals()
+export default injectGlobals
--- src/inject-globals.ts
+++ src/inject-globals.ts
@@ -1,39 +1,49 @@
 import path from 'path'
+import { TextsNodeGlobals } from '@textshq/platform-sdk/dist/TextsGlobals'
+
 import { USER_AGENT } from './constants'
 import { createHttpClient } from './got-fetch'
 import { fetch, fetchStream } from './fetch'
+// import { runOtherWorker } from './workers'
 
 const IS_DEV = true
+const isLoggingEnabled = true
 
-export default function injectGlobals() {
+export default function injectGlobals(userDataDirPath: string) {
   // globalThis.AsyncSqlite = AsyncSqlite
 
   globalThis.texts = {
     IS_DEV,
+    isLoggingEnabled,
     log: (...args: any[]) => {
-      if (!IS_DEV) return
+      if (!isLoggingEnabled) return
       console.log(...args)
     },
     error: (...args: any[]) => {
-      if (!IS_DEV) return
+      if (!isLoggingEnabled) return
       console.error(...args)
     },
     constants: {
       USER_AGENT,
-      // APP_VERSION: pkg.version,
+      APP_VERSION: '1.0.0',
     },
     Sentry: {
       captureException: () => {},
       captureMessage: () => {},
       startTransaction: () => {},
     },
-    trackPlatformEvent: () => {},
+    async trackPlatformEvent(data: any) {},
     getBinariesDirPath(platformName: string) {
       return path.join(__dirname, '../..', 'platform-' + platformName, 'binaries')
     },
-    // runWorker: runOtherWorker,
     fetch,
     fetchStream,
     createHttpClient,
-  }
+    nativeFetch: undefined as any,
+    nativeFetchStream: undefined as any,
+    runWorker: undefined as any,
+    forkChildProcess: undefined as any,
+    getOriginalObject: undefined as any,
+    openBrowserWindow: undefined as any,
+  } satisfies TextsNodeGlobals
 }
